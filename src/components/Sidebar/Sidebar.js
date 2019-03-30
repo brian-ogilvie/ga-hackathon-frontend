73a21d13b838b795bd267357b2d1949846ef6aae
Brian Ogilvie <brian@brianogilviedesigns.com>
2019-03-30 19:27:54 -0400

    layout for sidebar list items

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -13,11 +13,16 @@ class Sidebar extends React.Component {
     return (
       <div className="Sidebar">
         <div className="sidebar__content">
-          <h3 className="sidebar__heading">Data Analytics</h3>
-          <ul>
-            <li><Link to="/owners/1">Owner 1</Link></li>
-            <li><Link to="/owners/2">Owner 2</Link></li>
-            <li><Link to="/owners/3">Owner 3</Link></li>
+          <h3 className="sidebar__heading">Fleet</h3>
+          <ul className="sidebar__list">
+            <li className="sidebar__list-item sidebar__list-item--active"><Link to="/owners/1">Owner 1</Link></li>
+            <li className="sidebar__list-item"><Link to="/owners/2">Owner 2</Link></li>
+            <li className="sidebar__list-item"><Link to="/owners/3">Owner 3</Link></li>
+          </ul>
+          <h3 className="sidebar__heading">Settings</h3>
+          <ul className="sidebar__list">
+            <li className="sidebar__list-item">Adjust KPIs</li>
+            <li className="sidebar__list-item">Add New Owner</li>
           </ul>
         </div>
         <ExportReport />
